1fe50881a8baf86b4152ed546ac38654e5cbd655
Grzegorz Waszkowiak <grzegorz.waszkowiak@modernfactory.pl>
2016-12-21 21:46:42 +0100

    1-4

--- books/models.py
+++ books/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
+from djmoney.models.fields import MoneyField
 
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+    price = MoneyField(max_digits=8, decimal_places=2, default_currency='PLN')
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title + ' ' + self.author
